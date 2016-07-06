5f0b7a456be37e7b6185097e69bea6ba580a86ce
lance <lancedai@lagou.com>
2016-07-06 16:36:51 +0800

    Revert "...."
    This reverts commit a7615156626dbbb31e9481d015620e4102ab2205.

--- modules/Confirm/confirm.jsx
+++ modules/Confirm/confirm.jsx
@@ -88,7 +88,7 @@ class Confirm extends React.Component {
 
 Confirm.propTypes = {
 	text:PropTypes.string,
-	btnTpl:PropTypes.string,
+	btnTpl:PropTypes.string.isRequired,
 	btnType:PropTypes.string,
 	clickOutClose:PropTypes.bool,
 	style:PropTypes.object,
